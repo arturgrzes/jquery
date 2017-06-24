de06e0a9a745d9ffac8cfe195b9e906cde1321c4
Artur Grzes <grzesa@poczta.onet.pl>
2017-06-24 14:30:52 +0200

    2

---
+++ jquery/selektory.js
@@ -0,0 +1,17 @@
+'use strict'
+
+$(function() {
+    
+    var paragraphs = $('p');
+    console.log(paragraphs);
+    
+    
+    var paragraphOne = $('paragraf');
+    console.log(paragraphOne);
+    
+    
+    var paragraphTwo = $('paragraf');
+    console.log(paragraphTwo);
+    
+    
+});
